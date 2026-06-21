10586378fdc0bd11d8fa99eefcf345259159af59
Sameer Sundresh <sameer@sundresh.org>
2026-06-21 00:10:56 -0600

    Add `heloloop.asm` demonstrating that `int 0x21` re-enables interrupts during its execution

---
+++ src/hello/heloloop.asm
@@ -0,0 +1,24 @@
+org 0x100
+
+start:
+	; Notice that ^C still works in the loop below because `int 0x21` re-enables interrupts for
+	; most of the duration of its execution.
+	cli
+	mov	si, 0			; Loop has to run 64k times
+.loop:
+	mov	ah, 0x09
+	mov	dx, hello_string
+	int	0x21
+	mov	dx, there_string
+	int	0x21
+	dec	si
+	jnz	.loop
+
+	mov	ah, 0x00
+	int	0x21
+
+hello_string:
+	db	`Hello\r\n$`
+
+there_string:
+	db	`there.\r\n$`
