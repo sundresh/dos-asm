fb98eec8b1d6233c75d7872deb92f686285c74ef
Sameer Sundresh <sameer@sundresh.org>
2026-07-01 15:04:51 -0600

    Make hexdump32.c have a separate `-DTEST` mode

--- src/hello/hexdump32.c
+++ src/hello/hexdump32.c
@@ -1,14 +1,12 @@
-//#include <stdio.h>
-
-// To compile, use something like:
-// cc -S -O3 -masm=intel -m32 -nostdlib -ffreestanding -fno-pie -no-pie -fno-asynchronous-unwind-tables hexdump32.c
+// To compile to asm, use something like:
+//   cc -S -Os -masm=intel -m32 -nostdlib -ffreestanding -fno-pie -no-pie -fno-asynchronous-unwind-tables hexdump32.c
+// To compile for testing:
+//   cc -DTEST hexdump32.c -o hexdump32.test
 #include <stddef.h>
 #include <stdint.h>
-// Define types here because I don't have -m32 versions of the above headers installed
-//typedef unsigned char uint8_t;
-//typedef unsigned int uint32_t;
-//typedef uint32_t size_t;
-//typedef uint32_t uintptr_t;
+#ifdef TEST
+#include <stdio.h>
+#endif
 
 extern void print_string(size_t len, const char *str);
 extern void move_cursor_to_next_line();
@@ -25,23 +23,39 @@ void u8_to_hex(uint8_t x, char *buf) {
 }
 
 void u32_to_hex(uint32_t x, char *buf) {
-    u8_to_hex(x >> 24, buf);
-    u8_to_hex((x >> 16) & 0xff, buf+2);
-    u8_to_hex((x >> 8) & 0xff, buf+4);
-    u8_to_hex(x & 0xff, buf+6);
+    for (int i = 0; i < sizeof(x); i++) {
+        u8_to_hex((x >> 8*(sizeof(x) - i - 1)) & 0xff, buf+(2*i));
+    }
+}
+
+void u64_to_hex(uint64_t x, char *buf) {
+    for (int i = 0; i < sizeof(x); i++) {
+        u8_to_hex((x >> 8*(sizeof(x) - i - 1)) & 0xff, buf+(2*i));
+    }
+}
+
+void ptr_to_hex(void *p, char *buf) {
+#if UINTPTR_MAX == 0xffffffff
+    u32_to_hex((uintptr_t) p, buf);
+#elif UINTPTR_MAX == 0xffffffffffffffffULL
+    u64_to_hex((uintptr_t) p, buf);
+#else
+    #error "Unknown pointer size or unsupported architecture"
+#endif
 }
 
 // Prints data in hexadecimal, with lines of the form:
 //   0x[32-bit hex address]: [16 hex bytes, e.g., 1a b2 3c d4 ...]
 void hexdump(size_t len, uint8_t *data) {
-    char output_buf[80] = "0x";
+    char out_buf[80] = "0x";
+    // Address of the nearest multiple of 16 bytes at or before data
     uint8_t *in_ptr = (uint8_t *) (((uintptr_t) data) & ~0x0f);
     while (in_ptr < data+len) {
         // "0x"
-        char *out_ptr = output_buf + 2;
+        char *out_ptr = out_buf + 2;
         // "0x12345678"
-        u32_to_hex((uintptr_t) in_ptr, out_ptr);
-        out_ptr += 8;
+        ptr_to_hex(in_ptr, out_ptr);
+        out_ptr += 2*sizeof(uintptr_t);
         // "0x12345678:"
         *out_ptr = ':';
         out_ptr++;
@@ -57,11 +71,12 @@ void hexdump(size_t len, uint8_t *data) {
             }
             in_ptr++;
         }
-        print_string(out_ptr - output_buf, output_buf);
+        print_string(out_ptr - out_buf, out_buf);
         move_cursor_to_next_line();
     }
 }
-/*
+
+#ifdef TEST
 extern void print_string(size_t len, const char *str) {
     for (int i = 0; i < len; i++) {
         putchar(str[i]);
@@ -75,4 +90,4 @@ extern void move_cursor_to_next_line() {
 int main(int argc, char **argv) {
     hexdump(17, (uint8_t *) argv);
 }
-*/
+#endif
