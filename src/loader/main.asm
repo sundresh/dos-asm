98abd8e376166ac99f624c16e5fd187207a4275b
Sameer Sundresh <sameer@sundresh.org>
2026-07-26 00:04:20 -0600

    Enable/disable A20 line in loader main

--- src/loader/main.asm
+++ src/loader/main.asm
@@ -2,11 +2,16 @@
 [bits 16]
 
 main:
+	mov	ax, (BIOS_A20_AH << 8) | BIOS_A20_ENABLE_AL
+	int	BIOS_A20_INT
 	call	load_address_range_descriptors
 	call	call_main32
+	mov	ax, (BIOS_A20_AH << 8) | BIOS_A20_DISABLE_AL
+	int	BIOS_A20_INT
 	xor	ah, ah
 	int	0x21
 
-%include "smap.asm"
-%include "protmode.asm"
+%include "a20.inc"
 %include "main32.asm"
+%include "protmode.asm"
+%include "smap.asm"
